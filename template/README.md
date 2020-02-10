e930876361872c766df5119295a750e50c901973
Revath S Kumar <rsk@revathskumar.com>
2020-02-10 23:50:04 +0530

    feat: add template

---
+++ template/README.md
@@ -0,0 +1,7 @@
+## name
+
+## Usage
+
+## Setup
+
+## License
---
+++ template/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile('README.md', 'README.md', (err) => {
+  if (err) throw err;
+  console.log('README.md was copied');
+});
