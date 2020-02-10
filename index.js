26e6541e83d94ddc5f0f656198bdb67b89b2b0fd
Revath S Kumar <rsk@revathskumar.com>
2020-02-10 23:50:46 +0530

    feat: copy files + npm & git init

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const process = require("process");
+const mkdirp = require("mkdirp");
+const execSync = require("child_process").execSync;
+
+const args = process.argv.slice(2);
+const name = args[0];
+
+module.exports = function() {
+  if (!name) {
+    throw new Error("Name is required");
+  }
+  // check folder with name exists
+  mkdirp.sync(name);
+
+  console.log("create-unicorn :: ", __dirname, process.cwd());
+  const templateDir = `${__dirname}/template/`;
+  const projectDir = `${process.cwd()}/${name}/`;
+
+  mkdirp.sync(projectDir + "/.vscode");
+
+  fs.copyFile(
+    templateDir + "/.vscode/settings.json",
+    projectDir + "/.vscode/settings.json",
+    err => {
+      if (err) throw err;
+      console.log("/.vscode/settings.json  was copied");
+    }
+  );
+
+  const altNames = {
+    gitignore: ".gitignore"
+  };
+  ["README.md", "LICENSE", "index.js", ".editorconfig", "gitignore"].forEach(
+    fileName => {
+      destFileName = altNames[fileName] || fileName;
+
+      fs.copyFile(templateDir + fileName, projectDir + destFileName, err => {
+        if (err) throw err;
+        console.log(fileName + " was copied");
+      });
+    }
+  );
+  process.chdir(projectDir);
+  execSync("npm init -y");
+  execSync("git init");
+};
